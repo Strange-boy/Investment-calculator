e41be13f5f0dfc3d3afca8cd11786e550e8ae09f
Joel <joel.panvel@gmail.com>
2025-08-27 19:19:18 +0530

    Creating user-input component

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector : 'app-user-input',
+    standalone : true,
+    imports: [],
+    templateUrl : './user-input.component.html',
+    styleUrls : ['./user-component.component.css']
+})
+
+export class UserInputComponent {}
