1883748c1cd0747a5c60f7e7a42ec44f643ce270
Joel <joel.panvel@gmail.com>
2025-08-27 19:44:21 +0530

    Displaying the various data using two way data binding

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,25 @@
 import {Component} from "@angular/core";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     selector : 'app-user-input',
     standalone : true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl : './user-input.component.html',
     styleUrls : ['./user-component.component.css']
 })
 
-export class UserInputComponent {}
+export class UserInputComponent {
+    enteredInitialInvestment = '0';
+    enteredAnnualInvestment = '0';
+    enteredExpectedReturns = '3';
+    enteredDuration = '5';
+
+    onSubmit() : void {
+        console.log("Form submitted");
+        console.log(this.enteredAnnualInvestment);
+        console.log(this.enteredInitialInvestment);
+        console.log(this.enteredDuration);
+        console.log(this.enteredExpectedReturns)
+    }
+}
